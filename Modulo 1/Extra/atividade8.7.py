2bacc484661673da276976bc7ec85bbd15011731
vieirakelly1 <kellyvieira2208@gmail.com>
2025-09-10 17:19:50 -0300

    Delete Modulo 1/Extra/atividade8.7.py

--- Modulo 1/Extra/atividade8.7.py
+++
@@ -1,4 +0,0 @@
-# Dada a lista ['python', 'java', 'c++'], remova e retorne 'java' usando pop(índice).
-codigos = ['python','java','c++']
-remove = codigos.pop(2)
-print (f"Após pop(remove'{remove}'):",codigos)
