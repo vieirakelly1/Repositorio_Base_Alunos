070fb4738a81e57d7a7583e5383450cbb0ca86c9
vieirakelly1 <kellyvieira2208@gmail.com>
2025-09-10 16:59:38 -0300

    Delete Modulo 1/Listas/atividade7.5.py

--- Modulo 1/Listas/atividade7.5.py
+++
@@ -1,8 +0,0 @@
-# Crie uma função que receba um nome e um horário (manhã,
-# tarde, noite) e retorne uma saudação personalizada.
-
-from Atividades8.funcoes2 import periodo
-nome = input("Digite seu nome: ")
-horario = input("Digite um horário: ")
-
-periodo (nome,horario)
