2bcc0d9112e090588aba2b7b7233341e5cf09bf8
vieirakelly1 <kellyvieira2208@gmail.com>
2025-09-10 17:19:30 -0300

    Delete Modulo 1/Extra/atividade8.6.py

--- Modulo 1/Extra/atividade8.6.py
+++
@@ -1,5 +0,0 @@
-# Dada a lista [100, 200, 300, 400], remova e retorne o último elemento usando pop().
-centenas = [100,200,300,400]
-removido = centenas.pop(4)
-print (f"Após pop(removido'{removido}')",centenas)
-
