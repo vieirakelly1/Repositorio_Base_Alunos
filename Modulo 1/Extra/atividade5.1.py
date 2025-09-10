92231b94399443a87aebcf7c420c5f917bfabb23
vieirakelly1 <kellyvieira2208@gmail.com>
2025-09-10 16:44:36 -0300

    Add files via upload

---
+++ Modulo 1/Extra/atividade5.1.py
@@ -0,0 +1,9 @@
+#Contagem regressiva: Faça um programa que imprima uma contagem
+#regressiva de 10 até 1 e depois imprima "Fogo!".
+
+numero =11
+
+while numero>1:
+    numero = numero-1
+    print(numero)
+print("Fogo!")
