ff27bad353f0c0c6752a99462caeaebc782025e0
vieirakelly1 <kellyvieira2208@gmail.com>
2025-09-10 16:42:47 -0300

    Add files via upload

---
+++ Modulo 1/Listas/atividade7.3.py
@@ -0,0 +1,8 @@
+from funcoes import identidade
+nome= input("Digite seu nome:")
+idade= input("Digite sua idade: ")
+print(nome,idade)
+
+
+
+
---
+++ Modulo 1/Listas/atividade7.4.py
@@ -0,0 +1,13 @@
+# Exercício 1: Calculadora Simples
+# Enunciado: Crie uma função que receba dois números e uma operação (+, -,
+# *, /) e retorne o resultado da operação.
+
+from Atividades8.funcoes2 import operacao
+n4= int(input("Digite o primeiro número: "))
+n5= input("Digite um operador(+,-,* ou /): ")
+n6= int(input("Digite o segundo número: "))
+
+operacao(n4,n5,n6)
+
+
+
