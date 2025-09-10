0ae2b99e93f4d666df3ec740841d6841573ad34e
vieirakelly1 <kellyvieira2208@gmail.com>
2025-09-10 17:28:13 -0300

    Delete Modulo 1/Listas/desafio.py

--- Modulo 1/Listas/desafio.py
+++
@@ -1,7 +0,0 @@
-# Crie uma função chamada classificar_numero que recebe um número e retorna uma
-# mensagem com base no intervalo em que ele se encontra:
-from funcoes2 import classificar_numero
-n1 = int(input("Digite um numero: "))
-
-print(classificar_numero(n1))
-
