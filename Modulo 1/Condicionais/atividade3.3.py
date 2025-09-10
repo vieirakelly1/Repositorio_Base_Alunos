870e0fe036a16e475f9a5ed516deebc004e45150
vieirakelly1 <kellyvieira2208@gmail.com>
2025-09-10 16:40:00 -0300

    Add files via upload

---
+++ Modulo 1/Condicionais/atividade3.3.py
@@ -0,0 +1,9 @@
+numero = float(input("Digite um numero"))
+numero2 = float(input("Digite um numero"))
+
+if numero > numero2:
+    print("o maior numero é:",numero)
+
+else:
+    print("o maior numero é:",numero2)
+    
---
+++ Modulo 1/Condicionais/atividade3.5.py
@@ -0,0 +1,10 @@
+senha=input('digite a sua senha: ')
+
+senhaCorreta='1234'
+print==senhaCorreta
+
+if senha==senhaCorreta:
+    print("Acesso Permitido")
+
+else:
+    print("Acesso Negado")
